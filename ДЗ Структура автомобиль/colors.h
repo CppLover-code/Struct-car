565a139313abe77889d67f2f460ce58df9d8a7ca
Anna Boiko <annaboiko377@gmail.com>
2022-11-19 21:51:52 +0300

    Добавьте файлы проекта.

---
+++ ДЗ Структура автомобиль/colors.h
@@ -0,0 +1,74 @@
+#pragma once
+namespace colors {
+        const char* RESET  = "\033[0m";  	
+
+        // Regular Colors
+        const char* BLACK  = "\033[0;30m";   
+        const char* RED    = "\033[0;31m";     
+        const char* GREEN  = "\033[0;32m";   
+        const char* YELLOW = "\033[0;33m";  
+        const char* BLUE   = "\033[0;34m";    
+        const char* PURPLE = "\033[0;35m";  
+        const char* CYAN   = "\033[0;36m";    
+        const char* WHITE  = "\033[0;37m";   
+
+        // Bold
+        const char* BLACK_BOLD  = "\033[1;30m";  
+        const char* RED_BOLD    = "\033[1;31m";    
+        const char* GREEN_BOLD  = "\033[1;32m";  
+        const char* YELLOW_BOLD = "\033[1;33m"; 
+        const char* BLUE_BOLD   = "\033[1;34m";   
+        const char* PURPLE_BOLD = "\033[1;35m"; 
+        const char* CYAN_BOLD   = "\033[1;36m";   
+        const char* WHITE_BOLD  = "\033[1;37m";  
+
+        // Underline
+        const char* BLACK_UNDERLINED  = "\033[4;30m";  
+        const char* RED_UNDERLINED    = "\033[4;31m";    
+        const char* GREEN_UNDERLINED  = "\033[4;32m";  
+        const char* YELLOW_UNDERLINED = "\033[4;33m"; 
+        const char* BLUE_UNDERLINED   = "\033[4;34m";   
+        const char* PURPLE_UNDERLINED = "\033[4;35m"; 
+        const char* CYAN_UNDERLINED   = "\033[4;36m";   
+        const char* WHITE_UNDERLINED  = "\033[4;37m";  
+
+        // Background
+        const char* BLACK_BACKGROUND  = "\033[40m"; 
+        const char* RED_BACKGROUND    = "\033[41m";   
+        const char* GREEN_BACKGROUND  = "\033[42m"; 
+        const char* YELLOW_BACKGROUND = "\033[43m";
+        const char* BLUE_BACKGROUND   = "\033[44m";  
+        const char* PURPLE_BACKGROUND = "\033[45m";
+        const char* CYAN_BACKGROUND   = "\033[46m";  
+        const char* WHITE_BACKGROUND  = "\033[47m"; 
+
+        // High Intensity
+        const char* BLACK_BRIGHT  = "\033[0;90m"; 
+        const char* RED_BRIGHT    = "\033[0;91m";   
+        const char* GREEN_BRIGHT  = "\033[0;92m"; 
+        const char* YELLOW_BRIGHT = "\033[0;93m";
+        const char* BLUE_BRIGHT   = "\033[0;94m";  
+        const char* PURPLE_BRIGHT = "\033[0;95m";
+        const char* CYAN_BRIGHT   = "\033[0;96m";  
+        const char* WHITE_BRIGHT  = "\033[0;97m"; 
+
+        // Bold High Intensity
+        const char* BLACK_BOLD_BRIGHT  = "\033[1;90m"; 
+        const char* RED_BOLD_BRIGHT    = "\033[1;91m";   
+        const char* GREEN_BOLD_BRIGHT  = "\033[1;92m"; 
+        const char* YELLOW_BOLD_BRIGHT = "\033[1;93m";
+        const char* BLUE_BOLD_BRIGHT   = "\033[1;94m";  
+        const char* PURPLE_BOLD_BRIGHT = "\033[1;95m";
+        const char* CYAN_BOLD_BRIGHT   = "\033[1;96m";  
+        const char* WHITE_BOLD_BRIGHT  = "\033[1;97m"; 
+
+        // High Intensity backgrounds
+        const char* BLACK_BACKGROUND_BRIGHT  = "\033[0;100m";
+        const char* RED_BACKGROUND_BRIGHT    = "\033[0;101m";
+        const char* GREEN_BACKGROUND_BRIGHT  = "\033[0;102m";
+        const char* YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";
+        const char* BLUE_BACKGROUND_BRIGHT   = "\033[0;104m";
+        const char* PURPLE_BACKGROUND_BRIGHT = "\033[0;105m";
+        const char* CYAN_BACKGROUND_BRIGHT   = "\033[0;106m";
+        const char* WHITE_BACKGROUND_BRIGHT  = "\033[0;107m";    
+}
---
+++ ДЗ Структура автомобиль/ДЗ Структура автомобиль.cpp
@@ -0,0 +1,242 @@
+﻿/*
+    Реализовать структуру "Машина" (цвет, модель, номер).
+    Номер машины может представлять из себя или пятизначный
+    номер или слово до 8 символов(рекомендовано номер реализовать как
+    объединение). Реализовать заполнение и вывод данных.
+    Создать массив из 10 экземпляров структуры "Машина" =>
+    редактировать, вывести на экран все машины,поиск машины
+    по номеру.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include "colors.h"
+
+struct car
+{
+    std::string color;
+    std::string model;
+    std::string number;
+};
+
+car* arr;
+const int size = 10;
+
+void input_arr()                                                 // функция заполнения массива данными об авто
+{
+    arr = new car[size];                                         // выделение памяти под массив 
+
+    for (int i = 0; i < size; i++)                               // заполняем массив данными
+    {
+        std::cout << colors::BLUE_BOLD
+            << " Car number "
+            << i + 1 << ".\n"
+            << colors::RESET;
+
+        std::cout << " Enter color: ";
+        std::cin >> arr[i].color;
+
+        std::cout << " Enter model: ";
+        std::cin >> arr[i].model;
+
+        std::cout << " Enter number: ";
+        std::cin >> arr[i].number;
+
+    }
+}
+
+void print_rec(int i)                                           // функция вывода данных массива
+{
+    if (size < 1) std::cout << colors::RED_BRIGHT               // если размер массива меньше 1, то массив не заполнен
+        << " Car details not yet filled!"
+        << colors::RESET << "\n";
+    else                                                        // иначе выводим данные в консоль
+    {
+        std::cout << colors::BLUE_BOLD
+            << " Car number "
+            << i + 1 << "\n"
+            << colors::RESET
+
+            << " Color           " << arr[i].color << "\n"
+            << " Model           " << arr[i].model << "\n"
+            << " Number          " << arr[i].number << "\n\n";
+    }
+}
+
+void output_arr()                                               // функция вывода данных массива на экран консоли
+{
+    if (size <= 0) std::cout << colors::RED_BRIGHT              // если размер массива меньше <= 0, то массив не заполнен
+        << " Car details not yet filled!"
+        << colors::RESET << "\n";
+
+    else for (int i = 0; i < size; i++) print_rec(i);           // иначе выводим на экран консоли
+}
+
+int search()                                                    // функция поиска автомобиля по данным
+{
+
+    if (size < 1) std::cout << colors::RED_BRIGHT               // если размер массива меньше < 1, то массив не заполнен
+        << " Car details not yet filled!"
+        << colors::RESET << "\n";
+    else                                                        // иначе предлагаем выбрать характеристику для поиска
+    {
+        //system("pause");
+        Sleep(2000);
+        system("cls");
+
+        std::cout << " Enter number to search\n";
+
+        std::string data;
+        std::cin >> data;
+
+        int count_s = 0;                                         // счетчик кол-ва найденных авто
+
+        for (int i = 0; i < size; i++)
+        {
+            std::string temp;
+
+            temp = arr[i].number;
+
+            if (temp == data)                                    // вывод, если совпадение нашлось
+            {
+                print_rec(i);
+                count_s++;
+            }
+        }
+        return count_s;                                          // возвращаем найденное кол-во авто
+    }
+    return 0;
+}
+void editCar()                                                   // функция редактирования данных об автомобиле
+{
+    int carChoice;                                               // переменная для хранения порядкового номера авто
+    int editChoice;                                              // переменная для хранения выбранного параметра для редактирования
+
+    std::string newData;                                         // переменная для ввода нового значения для параметра
+
+    output_arr();                                                // выводим список авто со всеми параметрами
+
+    if (size < 1) std::cout << colors::RED_BRIGHT                // если размер массива меньше < 1, то массив не заполнен
+        << " Car details not yet filled!"
+        << colors::RESET << "\n";
+    else                                                         // иначе, выполняем редактирование
+    {
+        bool flag = true;
+        do
+        {
+            std::cout << std::endl << " Enter the serial number of car:\n";
+            std::cin >> carChoice;
+
+            if (carChoice > 10 || carChoice < 1)                  // если введенного порядкового номера не существует
+            {
+                std::cout << " Incorrect value!\n";               // выводим сообзение об ошибке, и предлагаем ввести номер еще раз
+            }
+            else flag = false;                                    // иначе, меняем flag и выходим из цикла
+
+        } while (flag);
+
+        carChoice--;                                              // уменьшаем порядковый номер авто на 1, 
+        // чтоб обратиться к данным авто по правильному индексу
+
+        std::cout << " Enter edit type: " << std::endl
+            << " 1 - color" << std::endl
+            << " 2 - model" << std::endl
+            << " 3 - number" << std::endl;
+        std::cin >> editChoice;
+
+        switch (editChoice)
+        {
+        case 1:
+            std::cout << " Enter a new car color:\n";
+            std::cin >> newData;
+            arr[carChoice].color = newData;
+            break;
+
+        case 2:
+            std::cout << " Enter a new car model:\n";
+            std::cin >> newData;
+            arr[carChoice].model = newData;
+            break;
+
+        case 3:
+            std::cout << " Enter a new car number:\n";
+            std::cin >> newData;
+            arr[carChoice].number = newData;
+            break;
+
+        default:
+            std::cout << " Incorrect value!" << std::endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    system("title Cars data");
+
+    bool work = true;
+    int menu;
+
+    do
+    {
+        std::cout << "MENU\n"
+            << "1 - add car details\n"
+            << "2 - show car details\n"
+            << "3 - data search\n"
+            << "4 - data edit\n"
+            << "Any key - exit\n"
+            << "Choose an option: ";
+        std::cin >> menu;
+
+        //system("pause");
+        Sleep(2000);
+        system("cls");
+
+        switch (menu)                                                        // проверяем ввод пользователя
+        {                                                                    // и выполняем соотв. функции и действия
+        case 1:
+
+            input_arr();
+            break;
+
+        case 2:
+
+            output_arr();
+            Sleep(2000);
+            break;
+
+        case 3:
+        {
+            int count = search();
+            (count < 1) ? std::cout << colors::RED_BRIGHT                    // если функция search() вернула значение < 1, то
+                << " Car data not found!"                                    // совпадений не найдено
+                << colors::RESET << "\n" :
+
+                std::cout << colors::GREEN_BRIGHT                            // иначе показываем кол-во найденных авто
+                << "Total found: " << count
+                << " car(s)" << colors::RESET << "\n";
+            Sleep(2000);
+            break;
+        }
+        case 4:
+
+            editCar();
+            break;
+        default:
+
+            work = false;
+            break;
+        }
+
+        //system("pause");
+        Sleep(2000);
+        system("cls");
+
+    } while (work);
+
+    delete[] arr;                                                             // освобождение памяти дин. массива
+
+    return 0;
+}
